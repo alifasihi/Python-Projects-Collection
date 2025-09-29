e63f709bbee0ae65e43f88e416e271783d7aee63
Ali Fasihi <im.alifasihi@gmail.com>
2025-09-29 15:01:33 +0330

    add Websites Checker project, create csv file for Websites Checker project

---
+++ Websites Checker.py
@@ -0,0 +1,49 @@
+import csv
+import requests
+from http import HTTPStatus
+from fake_useragent import UserAgent
+
+
+def get_website(csv_path: str) -> list[str]:
+    websites: list[str] = []
+    with open(csv_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if 'https://' not in row[1]:
+                websites.append(f'https://{row[1]}')
+            else:
+                websites.append(row[1])
+    return websites
+
+
+def get_user_agent() -> str:
+    ua = UserAgent()
+    return ua.chrome
+
+
+def get_status_description(status_code: int) -> str:
+    for value in HTTPStatus:
+        if value == status_code:
+            description: str = f'({value} {value.name}) {value.description}'
+            return description
+    return '(???) unknown status code'
+
+
+def check_website(website: str, user_agent: str):
+    try:
+        code: int = requests.get(website, headers={'user_agent': user_agent}).status_code
+        print(website, get_status_description(code))
+    except Exception:
+        print(f'***Could not get in information for website: {website}')
+
+
+def main():
+    sites: list[str] = get_website('websites.csv')
+    user_agent = get_user_agent()
+
+    for site in sites:
+        check_website(site, user_agent)
+
+
+if __name__ == '__main__':
+    main()
