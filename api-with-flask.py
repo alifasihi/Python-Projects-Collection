60359bd3944d81fac23fa418416621f1307f9916
Ali Fasihi <im.alifasihi@gmail.com>
2025-10-04 21:47:59 +0330

    add api-with-flask project

---
+++ api-with-flask.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from random import randint, choice
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    phrases: list[str] = ['Welcome to this page', 'you are looking good today',
+                          'the weather is great']
+    return {'phrase': choice(phrases),
+            'date': datetime.now()}
+
+
+@app.route('/api/random')
+def random():
+    number_input = request.args.get('number', type=int)
+    text_input = request.args.get('text', type=str)
+
+    if isinstance(number_input, int):
+        return {
+            'input': number_input,
+            'random': randint(0, number_input),
+            'text': text_input,
+            'date': datetime.now()
+        }
+    else:
+        return {'Error': 'Please only enter number'}
+
+
+if __name__ == '__main__':
+    app.run()
